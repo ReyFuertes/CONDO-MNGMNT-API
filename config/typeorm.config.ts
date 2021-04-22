2973b19dc469a50062a038645f005e9d4d3f2bc2
Rfuertes <reynelfuertes@gmail.com>
2021-04-22 23:14:54 +0800

    change connection

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -2,14 +2,28 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 const SnakeNamingStrategy = require('typeorm-naming-strategies')
   .SnakeNamingStrategy;
 
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'p@55w0rd',
+//   database: 'cma',
+//   entities: [__dirname + 'dist/../**/*.entity.{js,ts}'],
+//   synchronize: false,
+//   logging: false,
+//   namingStrategy: new SnakeNamingStrategy(),
+// }
+
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'localhost',
+  host: 'cma-db-server-v1.postgres.database.azure.com',
   port: 5432,
-  username: 'postgres',
-  password: 'p@55w0rd',
+  username: "cmadevapiv1admin@cma-db-server-v1",
+  password: '7c74a169-81d9-4e9b-90a7-efba9721f05a',
   database: 'cma',
   entities: [__dirname + 'dist/../**/*.entity.{js,ts}'],
+  ssl: true,
   synchronize: true,
   logging: false,
   namingStrategy: new SnakeNamingStrategy(),
