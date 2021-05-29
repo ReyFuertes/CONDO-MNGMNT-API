1264a85443da51c7913a26191495c2d35d7f8204
Rfuertes <reynelfuertes@gmail.com>
2021-05-30 05:26:51 +0800

    remove partner

--- src/partner/partner.module.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([])],
-  controllers: [],
-  providers: []
-})
-export class PartnerModule { }
--- src/partner/partner.repository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository, getRepository, getCustomRepository } from 'typeorm';
-import { Partner } from './partner.entity';
-
-@EntityRepository(Partner)
-export class PartnerRepository extends Repository<Partner> {
-
-}
